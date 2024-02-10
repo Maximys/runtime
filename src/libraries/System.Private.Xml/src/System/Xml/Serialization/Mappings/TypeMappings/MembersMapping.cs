c5a0ec0994e2941f974f77a0d7be1d37e6bc80a9
Maksim Golev <mixim33@yandex.ru>
2024-02-10 22:13:51 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.TypeMappings" namespace.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/TypeMappings/MembersMapping.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Xml.Serialization.Mappings.TypeMappings
+{
+    internal sealed class MembersMapping : TypeMapping
+    {
+        internal MemberMapping[]? Members { get; set; }
+
+        internal MemberMapping? XmlnsMember { get; set; }
+
+        internal bool HasWrapperElement { get; set; } = true;
+
+        internal bool ValidateRpcWrapperElement { get; set; }
+
+        internal bool WriteAccessors { get; set; } = true;
+    }
+}
