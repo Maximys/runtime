a6454099d6a42536bcd47d87e52b1ccbf2b12cbd
Maksim Golev <mixim33@yandex.ru>
2024-02-10 22:33:21 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.TypeMappings.PrimitiveMappings" namespace.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/TypeMappings/PrimitiveMappings/EnumMapping.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Xml.Serialization.Mappings.TypeMappings.PrimitiveMappings
+{
+    internal sealed class EnumMapping : PrimitiveMapping
+    {
+        internal bool IsFlags{ get; set; }
+
+        internal ConstantMapping[]? Constants { get; set; }
+    }
+}
