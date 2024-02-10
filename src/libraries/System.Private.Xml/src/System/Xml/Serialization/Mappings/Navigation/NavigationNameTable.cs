8ab655d416bb4f40448d09d4edcc21732b665e48
Maksim Golev <mixim33@yandex.ru>
2024-02-10 19:32:39 +0400

    refactor(_): Changing type of "System.Xml.Serialization.Types.TypeScope.s_primitiveNames" to generic.

--- src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/Navigation/NavigationNameTable.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/Navigation/NavigationNameTable.cs
@@ -74,4 +74,103 @@ namespace System.Xml.Serialization.Mappings.Navigation
             return a;
         }
     }
+
+    internal sealed class NavigationNameTable<T> : INavigationNameScope
+    {
+        private readonly Dictionary<NavigationNameKey, T> _table = new Dictionary<NavigationNameKey, T>();
+
+        internal void Add(XmlQualifiedName qname, T value)
+        {
+            Add(qname.Name, qname.Namespace, value);
+        }
+
+        internal void Add(string? name, string? ns, T value)
+        {
+            NavigationNameKey key = new NavigationNameKey(name, ns);
+            _table.Add(key, value);
+        }
+
+        internal T? this[XmlQualifiedName qname]
+        {
+            get
+            {
+                T? obj;
+                return _table.TryGetValue(new NavigationNameKey(qname.Name, qname.Namespace), out obj) ? obj : default;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case T valueT:
+                        _table[new NavigationNameKey(qname.Name, qname.Namespace)] = valueT;
+                        break;
+                    case null:
+                        _table.Remove(new NavigationNameKey(qname.Name, qname.Namespace));
+                        break;
+                    default:
+                        throw new InvalidCastException();
+                }
+            }
+        }
+
+        internal T? this[string? name, string? ns]
+        {
+            get
+            {
+                T? obj;
+                _table.TryGetValue(new NavigationNameKey(name, ns), out obj);
+                return obj;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case T valueT:
+                        _table[new NavigationNameKey(name, ns)] = valueT;
+                        break;
+                    case null:
+                        _table.Remove(new NavigationNameKey(name, ns));
+                        break;
+                    default:
+                        throw new InvalidCastException();
+                }
+            }
+        }
+
+        object? INavigationNameScope.this[string? name, string? ns]
+        {
+            get
+            {
+                T? obj;
+                _table.TryGetValue(new NavigationNameKey(name, ns), out obj);
+                return obj;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case T valueT:
+                        _table[new NavigationNameKey(name, ns)] = valueT;
+                        break;
+                    case null:
+                        _table.Remove(new NavigationNameKey(name, ns));
+                        break;
+                    default:
+                        throw new InvalidCastException();
+                }
+            }
+        }
+
+        internal ICollection<T> Values
+        {
+            get { return _table.Values; }
+        }
+
+        internal Array ToArray()
+        {
+            T[] a = new T[_table.Count];
+            _table.Values.CopyTo(a, 0);
+            return a;
+        }
+    }
 }
