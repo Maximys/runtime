c1a7f4387b45d985b8ab42ce8f2958c2955fdbce
Maksim Golev <mixim33@yandex.ru>
2024-02-10 22:33:21 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.TypeMappings.PrimitiveMappings" namespace.

--- src/libraries/System.Private.Xml/src/System/Xml/Serialization/ReflectionXmlSerializationReader.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/ReflectionXmlSerializationReader.cs
@@ -10,6 +10,7 @@ using System.Reflection;
 using System.Xml.Schema;
 using System.Xml.Serialization.Mappings.Accessors;
 using System.Xml.Serialization.Mappings.TypeMappings;
+using System.Xml.Serialization.Mappings.TypeMappings.PrimitiveMappings;
 using System.Xml.Serialization.Types;
 
 // UnconditionalSuppressMessage that specify a Target need to be at the assembly or module level for now. Also,
