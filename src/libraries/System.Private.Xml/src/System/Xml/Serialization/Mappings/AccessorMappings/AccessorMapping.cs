84912dc125a72b98c0017058fe8e0d9ed0f95e21
Maksim Golev <mixim33@yandex.ru>
2024-02-10 23:11:49 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.AccessorMappings" namespace.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/AccessorMappings/AccessorMapping.cs
@@ -0,0 +1,205 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Serialization.Mappings.Accessors;
+using System.Xml.Serialization.Types;
+
+namespace System.Xml.Serialization.Mappings.AccessorMappings
+{
+    internal abstract class AccessorMapping : Mapping
+    {
+        private ElementAccessor[]? _elements;
+        private ElementAccessor[]? _sortedElements;
+
+        internal AccessorMapping()
+        { }
+
+        protected AccessorMapping(AccessorMapping mapping)
+            : base(mapping)
+        {
+            TypeDesc = mapping.TypeDesc;
+            Attribute = mapping.Attribute;
+            _elements = mapping._elements;
+            _sortedElements = mapping._sortedElements;
+            Text = mapping.Text;
+            ChoiceIdentifier = mapping.ChoiceIdentifier;
+            Xmlns = mapping.Xmlns;
+            Ignore = mapping.Ignore;
+        }
+
+        internal bool IsAttribute
+        {
+            get { return Attribute != null; }
+        }
+
+        internal bool IsText
+        {
+            get { return Text != null && (_elements == null || _elements.Length == 0); }
+        }
+
+        internal bool IsParticle
+        {
+            get { return (_elements != null && _elements.Length > 0); }
+        }
+
+        internal TypeDesc? TypeDesc { get; set; }
+
+        internal AttributeAccessor? Attribute { get; set; }
+
+        internal ElementAccessor[]? Elements
+        {
+            get { return _elements; }
+            set { _elements = value; _sortedElements = null; }
+        }
+
+        internal static void SortMostToLeastDerived(ElementAccessor[] elements)
+        {
+            Array.Sort(elements, new AccessorComparer());
+        }
+
+        internal sealed class AccessorComparer : IComparer<ElementAccessor>
+        {
+            public int Compare(ElementAccessor? a1, ElementAccessor? a2)
+            {
+                if (a1 == a2)
+                {
+                    return 0;
+                }
+
+                Debug.Assert(a1 != null);
+                Debug.Assert(a2 != null);
+                int w1 = a1.Mapping!.TypeDesc!.Weight;
+                int w2 = a2.Mapping!.TypeDesc!.Weight;
+                if (w1 == w2)
+                {
+                    return 0;
+                }
+                if (w1 < w2)
+                {
+                    return 1;
+                }
+
+                return -1;
+            }
+        }
+
+        internal ElementAccessor[]? ElementsSortedByDerivation
+        {
+            get
+            {
+                if (_sortedElements != null)
+                {
+                    return _sortedElements;
+                }
+
+                if (_elements == null)
+                {
+                    return null;
+                }
+
+                _sortedElements = new ElementAccessor[_elements.Length];
+                Array.Copy(_elements, _sortedElements, _elements.Length);
+                SortMostToLeastDerived(_sortedElements);
+                return _sortedElements;
+            }
+        }
+
+        internal TextAccessor? Text { get; set; }
+
+        internal ChoiceIdentifierAccessor? ChoiceIdentifier { get; set; }
+
+        internal XmlnsAccessor? Xmlns { get; set; }
+
+        internal bool Ignore { get; set; }
+
+        internal Accessor? Accessor
+        {
+            get
+            {
+                if (Xmlns != null)
+                {
+                    return Xmlns;
+                }
+
+                if (Attribute != null)
+                {
+                    return Attribute;
+                }
+
+                if (_elements != null && _elements.Length > 0)
+                {
+                    return _elements[0];
+                }
+
+                return Text;
+            }
+        }
+
+        internal static bool ElementsMatch(ElementAccessor[]? a, ElementAccessor[]? b)
+        {
+            if (a == null)
+            {
+                if (b == null)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            if (b == null)
+            {
+                return false;
+            }
+
+            if (a.Length != b.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Name != b[i].Name || a[i].Namespace != b[i].Namespace || a[i].Form != b[i].Form || a[i].IsNullable != b[i].IsNullable)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        internal bool Match(AccessorMapping mapping)
+        {
+            if (Elements != null && Elements.Length > 0)
+            {
+                if (!ElementsMatch(Elements, mapping.Elements))
+                {
+                    return false;
+                }
+
+                if (Text == null)
+                {
+                    return (mapping.Text == null);
+                }
+            }
+
+            if (Attribute != null)
+            {
+                if (mapping.Attribute == null)
+                {
+                    return false;
+                }
+
+                return (Attribute.Name == mapping.Attribute.Name && Attribute.Namespace == mapping.Attribute.Namespace && Attribute.Form == mapping.Attribute.Form);
+            }
+
+            if (Text != null)
+            {
+                return (mapping.Text != null);
+            }
+
+            return (mapping.Accessor == null);
+        }
+    }
+}
---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/AccessorMappings/MemberMapping.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection;
+
+namespace System.Xml.Serialization.Mappings.AccessorMappings
+{
+    internal sealed class MemberMapping : AccessorMapping
+    {
+        private string? _name;
+
+        internal MemberMapping() { }
+
+        private MemberMapping(MemberMapping mapping)
+            : base(mapping)
+        {
+            _name = mapping._name;
+            CheckShouldPersist = mapping.CheckShouldPersist;
+            CheckSpecified = mapping.CheckSpecified;
+            IsReturnValue = mapping.IsReturnValue;
+            ReadOnly = mapping.ReadOnly;
+            SequenceId = mapping.SequenceId;
+            MemberInfo = mapping.MemberInfo;
+            CheckSpecifiedMemberInfo = mapping.CheckSpecifiedMemberInfo;
+            CheckShouldPersistMethodInfo = mapping.CheckShouldPersistMethodInfo;
+        }
+
+        internal bool CheckShouldPersist { get; set; }
+
+        internal SpecifiedAccessor CheckSpecified { get; set; }
+
+        internal string Name
+        {
+            get { return _name ?? string.Empty; }
+            set { _name = value; }
+        }
+
+        internal MemberInfo? MemberInfo{ get; set; }
+
+        internal MemberInfo? CheckSpecifiedMemberInfo { get; set; }
+
+        internal MethodInfo? CheckShouldPersistMethodInfo { get; set; }
+
+        internal bool IsReturnValue { get; set; }
+
+        internal bool ReadOnly { get; set; }
+
+        internal bool IsSequence
+        {
+            get { return SequenceId >= 0; }
+        }
+
+        internal int SequenceId { get; set; } = -1;
+
+        internal MemberMapping Clone()
+        {
+            return new MemberMapping(this);
+        }
+    }
+}
