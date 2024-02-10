457b761a9b3abcfcc6ced521cce6c599d63c3121
Maksim Golev <mixim33@yandex.ru>
2024-02-10 23:26:11 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.Accessors.Comparers" namespace.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/Accessors/Comparers/AccessorComparer.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace System.Xml.Serialization.Mappings.Accessors.Comparers
+{
+    internal sealed class AccessorComparer : IComparer<ElementAccessor>
+    {
+        public int Compare(ElementAccessor? a1, ElementAccessor? a2)
+        {
+            if (a1 == a2)
+            {
+                return 0;
+            }
+
+            Debug.Assert(a1 != null);
+            Debug.Assert(a2 != null);
+            int w1 = a1.Mapping!.TypeDesc!.Weight;
+            int w2 = a2.Mapping!.TypeDesc!.Weight;
+
+            if (w1 == w2)
+            {
+                return 0;
+            }
+
+            if (w1 < w2)
+            {
+                return 1;
+            }
+
+            return -1;
+        }
+    }
+}
