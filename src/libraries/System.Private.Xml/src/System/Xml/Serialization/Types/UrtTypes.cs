fe65072a47898596e4a2cabf8cd2d2b3483757d9
Maksim Golev <mixim33@yandex.ru>
2024-02-10 12:56:38 +0400

    refactor(_): Extracting classes from "System.Private.Xml\src\System\Xml\Serialization\Types.cs" to separate namespace and files.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Types/UrtTypes.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Xml.Serialization.Types
+{
+    internal static class UrtTypes
+    {
+        internal const string Namespace = "http://microsoft.com/wsdl/types/";
+    }
+}
