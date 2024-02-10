ddb7e2032b1d4753d7e84c846fb9e6574da60998
Maksim Golev <mixim33@yandex.ru>
2024-02-10 20:25:56 +0400

    refactor(_): Creating "System.Xml.Serialization.Mappings.Accessors" namespace.

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/Accessors/AttributeAccessor.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Xml.Schema;
+
+namespace System.Xml.Serialization.Mappings.Accessors
+{
+    internal sealed class AttributeAccessor : Accessor
+    {
+        private bool _isSpecial;
+        private bool _isList;
+
+        internal bool IsSpecialXmlNamespace
+        {
+            get { return _isSpecial; }
+        }
+
+        internal bool IsList
+        {
+            get { return _isList; }
+            set { _isList = value; }
+        }
+
+        internal void CheckSpecial()
+        {
+            int colon = Name.LastIndexOf(':');
+
+            if (colon >= 0)
+            {
+                if (!Name.StartsWith("xml:", StringComparison.Ordinal))
+                {
+                    throw new InvalidOperationException(SR.Format(SR.Xml_InvalidNameChars, Name));
+                }
+                Name = Name.Substring("xml:".Length);
+                Namespace = XmlReservedNs.NsXml;
+                _isSpecial = true;
+            }
+            else
+            {
+                if (Namespace == XmlReservedNs.NsXml)
+                {
+                    _isSpecial = true;
+                }
+                else
+                {
+                    _isSpecial = false;
+                }
+            }
+            if (_isSpecial)
+            {
+                Form = XmlSchemaForm.Qualified;
+            }
+        }
+    }
+}
