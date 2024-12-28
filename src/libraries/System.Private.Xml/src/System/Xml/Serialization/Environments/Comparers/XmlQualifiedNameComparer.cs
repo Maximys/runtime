9bc8a8d25992a1887b5a4a1129b4ca1370f2a49b
Maksim Golev <mixim33@yandex.ru>
2024-12-28 09:26:41 +0400

    refactor(): Extracting common conditional logic of "XmlSerializationReader.GetPrimitiveType(XmlQualifiedName, bool)" to "XmlSerializationReader.TypeMappers".

---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Environments/Comparers/XmlQualifiedNameComparer.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Xml.Serialization.Environments.Comparers
+{
+    internal sealed class XmlQualifiedNameComparer : IEqualityComparer<XmlQualifiedName>
+    {
+        public bool Equals(XmlQualifiedName? x, XmlQualifiedName? y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if ((x is null) || (y is null))
+            {
+                return false;
+            }
+
+            if ((x.Name == y.Name) && (x.Namespace == y.Namespace))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode([DisallowNull] XmlQualifiedName obj)
+        {
+            return HashCode.Combine(
+                obj.Name,
+                obj.Namespace);
+        }
+    }
+}
---
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Mappings/Mapper.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Xml.Serialization.Mappings
+{
+    internal sealed class Mapper
+    {
+        public Type PrimitiveType { get; }
+
+        public Mapper(Type primitiveType)
+        {
+            PrimitiveType = primitiveType;
+        }
+    }
+}
